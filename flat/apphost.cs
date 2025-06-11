05dfff64c9ba0c3d1affaec355bc91ace7b54ffc
Damian Edwards <damian@damianedwards.com>
2025-06-11 07:24:35 -0700

    Updates

--- flat/apphost.cs
+++ flat/apphost.cs
@@ -1,15 +1,15 @@
 #!/usr/bin/dotnet run
 
 #:sdk Microsoft.NET.Sdk
-#:sdk Aspire.Hosting.Sdk 9.2.0
-#:package Aspire.Hosting.AppHost@9.2.0
-#:package Aspire.Hosting.Redis@9.2.0
+#:sdk Aspire.Hosting.Sdk 9.3.0
+#:package Aspire.Hosting.AppHost@9.3.0
+#:package Aspire.Hosting.Redis@9.3.0
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
 
-builder.AddDotnetApp("../webapi/webapi.cs")
-    .WithReference(redis).WaitFor(redis);
+// builder.AddDotnetApp("../webapi/webapi.cs")
+//     .WithReference(redis).WaitFor(redis);
 
 builder.Build.Run();
---
+++ flat/envvars.cs
@@ -0,0 +1,17 @@
+#:property DefineConstants $(DefineConstants);CUSTOM_CONSTANT;$(EnvVarConstant)
+
+#if DEBUG
+Console.WriteLine("Hello from Debug!");
+#endif
+
+#if RELEASE
+Console.WriteLine("Hello from Release!");
+#endif
+
+#if CUSTOM_CONSTANT
+Console.WriteLine("CUSTOM_CONSTANT is enabled!");
+#endif
+
+#if ENV_VAR_CONSTANT
+Console.WriteLine("EnvVarConstant is enabled!");
+#endif
