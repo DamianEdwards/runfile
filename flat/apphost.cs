2b699f3a4e74e5801519eb8982635cd990e831b7
Damian Edwards <damian@damianedwards.com>
2025-08-04 13:54:37 -0700

    More updates

--- flat/apphost.cs
+++ flat/apphost.cs
@@ -1,9 +1,10 @@
 #!/usr/bin/env dotnet
 
 #:sdk Microsoft.NET.Sdk
-#:sdk Aspire.Hosting.Sdk 9.3.0
-#:package Aspire.Hosting.AppHost@9.3.0
-#:package Aspire.Hosting.Redis@9.3.0
+#:sdk Aspire.AppHost.Sdk@9.4.0
+#:package Aspire.Hosting.AppHost@9.4.0
+#:package Aspire.Hosting.Redis@9.4.0
+#:property PublishAot=false
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -12,4 +13,4 @@ var redis = builder.AddRedis("redis");
 // builder.AddDotnetApp("../webapi/webapi.cs")
 //     .WithReference(redis).WaitFor(redis);
 
-builder.Build.Run();
+builder.Build().Run();
--- flat/hello.cs
+++ flat/hello.cs
@@ -1,4 +1,2 @@
-#!/usr/bin/env dotnet
-
 var name = args.Length > 0 ? args[0] : "World";
 Console.WriteLine($"Hello, {name}!");
