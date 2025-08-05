44dc73e16af54d4bda71ec3979025958e8834176
Damian Edwards <damian@damianedwards.com>
2025-08-05 16:42:25 -0700

    rc.1 updates

--- flat/apphost.cs
+++ flat/apphost.cs
@@ -1,9 +1,9 @@
 #!/usr/bin/env dotnet
 
 #:sdk Microsoft.NET.Sdk
-#:sdk Aspire.AppHost.Sdk 9.3.0
-#:package Aspire.Hosting.AppHost@9.3.0
-#:package Aspire.Hosting.Redis@9.3.0
+#:sdk Aspire.AppHost.Sdk@9.4.0
+#:package Aspire.Hosting.AppHost@9.4.0
+#:package Aspire.Hosting.Redis@9.4.0
 
 var builder = DistributedApplication.CreateBuilder(args);
 
