ab5028ec2ea2f067f1560275ec94fa7afd4b6513
Damian Edwards <damian@damianedwards.com>
2026-05-17 10:11:50 -0700

    update for 10.0.300 release (#14)

--- flat/cat.cs
+++ flat/cat.cs
@@ -1,15 +1,11 @@
 #!/usr/bin/env dotnet
 
-// Properties to enable experimental features for file-based apps
-// - include/exclude directives moving to non-experimental in 10.0.300
-// - transitive directives experimental status still being debated for 10.0.300
+// Transitive directives are experimental in 10.0.300
 // Note: These could be put in a `Directory.Build.props` file instead if preferred
-#:property ExperimentalFileBasedProgramEnableIncludeDirective=true
-#:property ExperimentalFileBasedProgramEnableExcludeDirective=true
 #:property ExperimentalFileBasedProgramEnableTransitiveDirectives=true
 
 // Include other files in the virtual project, item type is inferred from the file extension
-// Note: Using globs here is supported but effectively disables MSBuild caching atm
+// Note: Using globs here is supported but effectively disables MSBuild caching for now
 #:include ./cat.commands.cs
 
 var rootCommand = CatCli.Commands.DefineRootCommand();
