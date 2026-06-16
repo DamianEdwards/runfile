6dfd326181e32b72aa0cabc098618bc3ae6cd939
Damian Edwards <damian@damianedwards.com>
2026-06-15 17:19:54 -0700

    Updates

--- flat/apphost.cs
+++ flat/apphost.cs
@@ -1,19 +1,19 @@
 #!/usr/bin/env aspire
 
-#:sdk Aspire.AppHost.Sdk@13.0.0
-#:package Aspire.Hosting.Redis@13.0.0
+#:sdk Aspire.AppHost.Sdk@13.3.3
+//#:package Aspire.Hosting.Redis@13.3.3
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var webapi = builder.AddCSharpApp("webapi", "./webapi.cs");   
+var webapi = builder.AddCSharpApp("webapi", "./webapi.cs");
 
 builder.AddCSharpApp("razorapp", "../razorapp/razorapp.cs")
     .WithReference(webapi).WaitFor(webapi);
 
-if (!string.Equals(builder.Configuration["DOTNET_LAUNCH_PROFILE"], "verify", StringComparison.OrdinalIgnoreCase))
-{
-    var redis = builder.AddRedis("redis");
-    webapi.WithReference(redis).WaitFor(redis);
-}
+// if (!string.Equals(builder.Configuration["DOTNET_LAUNCH_PROFILE"], "verify", StringComparison.OrdinalIgnoreCase))
+// {
+//     var redis = builder.AddRedis("redis");
+//     webapi.WithReference(redis).WaitFor(redis);
+// }
 
 builder.Build().Run();
--- flat/cat.cs
+++ flat/cat.cs
@@ -1,9 +1,5 @@
 #!/usr/bin/env dotnet
 
-// Transitive directives are experimental in 10.0.300
-// Note: These could be put in a `Directory.Build.props` file instead if preferred
-#:property ExperimentalFileBasedProgramEnableTransitiveDirectives=true
-
 // Include other files in the virtual project, item type is inferred from the file extension
 // Note: Using globs here is supported but effectively disables MSBuild caching for now
 #:include ./cat.commands.cs
